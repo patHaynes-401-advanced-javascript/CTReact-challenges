56d27fe5164e162b2ec46338aafa6d2b29c66a66
Pat Haynes <pathaynes32@gmail.com>
2019-11-03 13:22:52 -0800

    merge sort testing

---
+++ Challenges/mergeSort.js
@@ -0,0 +1,45 @@
+module.export = function mergeSort(arr) {
+
+  let n = arr.length;
+  if(n > 1) {
+    let mid = Math.floor(n / 2);
+    let left = arr.slice(0, mid);
+    let right = arr.slice(mid, n);
+    mergeSort(left);
+    mergeSort(right);
+    merge(left, right, arr);
+  }
+};
+
+function merge(left, right, arr) {
+  let i = 0;
+  let j = 0;
+  let k = 0;
+
+  while(1 < left.length && j < right.length) {
+    if(left[i] <= right[j]) {
+      arr[k] = left[i];
+      i++;
+    }
+    else {
+      arr[k] = right[j];
+      j++;
+    }
+    k++;
+  }
+  if(i === left.length) {
+    while(j < right.length) {
+      arr[k] = right[j];
+      j++;
+      k++;
+    }
+  }
+  else {
+    while(i < left.length) {
+      arr[k] = left[i];
+      i++;
+      k++;
+    }
+  }
+}
+
