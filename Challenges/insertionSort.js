19c058bf79f6bc9a2213cb9079d1b49a4a2af092
Pat Haynes <pathaynes32@gmail.com>
2019-11-03 12:05:02 -0800

    setting up environment and first challenge

---
+++ Challenges/insertionSort.js
@@ -0,0 +1,14 @@
+module.exports = function insertionSort(arr) {
+
+  for(let i = 0; i < arr.length; i++) {
+    let j = i - 1;
+    let temp = arr[i];
+
+    while(j >= 0 && temp < arr[j]) {
+      arr[j + a] = arr[j];
+      j--;
+    }
+    arr[j + 1] = temp;
+  }
+  return arr;
+};
